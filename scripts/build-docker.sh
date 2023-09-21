fc8419c4f88d35fa9427ff3bfef4e115bc7111c2
ohaddahan <ohaddahan@gmail.com>
2023-09-21 21:24:46 +0300

    change to default location

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,4 +2,4 @@
 commit=$(git rev-parse --short HEAD)
 #git log --branches master -n 1 --format=oneline --abbrev-commit  | awk '{print $1}'
 name="rust-ci-cd-${commit}"
-docker build --tag "${name}" --file docker-files/build.dockerfile . && printf "\n\nBuilt image: ${name}\n"
+docker build --tag "${name}" --file Dockerfile . && printf "\n\nBuilt image: ${name}\n"
